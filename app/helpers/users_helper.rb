170a3eba48a5cfa87da64f5aed6094cdefbaf914
song940 <admin@lsong.org>
2013-09-02 13:56:49 +0800

    init to github

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+	# Returns the Gravatar (http://gravatar.com/) for the given user.
+  def gravatar_for(user, options = { size: 50 , class:'gravatar' })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{options[:size]}"
+    image_tag(gravatar_url, alt: user.name, class: options[:class])
+  end
 end
