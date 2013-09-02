5b9c80ca7d7e648ca999270e816fdcd425f07efc
song940 <admin@lsong.org>
2013-09-02 16:34:55 +0800

    fix assets pipline scss files .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,14 +33,14 @@ Hrm::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.assets.version = '2.0'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-   config.force_ssl = true
+	config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
