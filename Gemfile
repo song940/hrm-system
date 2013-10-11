28ca5bc690a4e10053b4665fea240a419ca9f3e3
song940 <admin@lsong.org>
2013-10-11 11:34:17 +0800

    add i18n

--- Gemfile
+++ Gemfile
@@ -74,3 +74,5 @@ group :development do
 #    gem 'sunspot_solr'
 
 end
+
+gem "font-awesome-rails"
--- config/application.rb
+++ config/application.rb
@@ -15,10 +15,13 @@ module Hrm
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.assets.enabled = true
+
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
-	#config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+	config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif app.css app.js)
+
   end
 end
